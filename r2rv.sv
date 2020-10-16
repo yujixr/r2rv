9ef92a8e8371d4a2dff1eb9452686f4efd2db24b
YUJI <mail@yuji.ne.jp>
2020-10-16 18:00:26 +0900

    Simple LED program

---
+++ r2rv.sv
@@ -0,0 +1,10 @@
+module r2rv(
+  input   logic reset,
+  input   logic [9:0] sw,
+  input   logic [2:0] key,
+  output  logic [9:0] led
+);
+
+  assign led = sw & ~key[0] & ~key[1] & ~key[2] & ~reset;
+
+endmodule
