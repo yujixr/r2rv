b3f90682d3201d1026a1c5bae92046ad698da6b1
YUJI <mail@yuji.ne.jp>
2020-10-31 15:27:32 +0900

    Fix 7SEG mapping

--- 7seg.sv
+++ 7seg.sv
@@ -5,23 +5,23 @@ module hex_display(
 
 always_comb
   case(src)
-    4'h0: segment = 7'b0000001;
-    4'h1: segment = 7'b1001111;
-    4'h2: segment = 7'b0010010;
-    4'h3: segment = 7'b0000110;
-    4'h4: segment = 7'b1001100;
-    4'h5: segment = 7'b0100100;
-    4'h6: segment = 7'b0100000;
-    4'h7: segment = 7'b0001111;
-    4'h8: segment = 7'b0000000;
-    4'h9: segment = 7'b0000100;
-    4'ha: segment = 7'b0001000;
-    4'hb: segment = 7'b1100000;
-    4'hc: segment = 7'b0110001;
-    4'hd: segment = 7'b1000010;
-    4'he: segment = 7'b0110000;
-    4'hf: segment = 7'b0111000;
-    default: segment = 7'b1111111;
+		4'h1: segment = 7'b1111001;   // ---t----
+		4'h2: segment = 7'b0100100; 	// |	  |
+		4'h3: segment = 7'b0110000; 	// lt	 rt
+		4'h4: segment = 7'b0011001; 	// |	  |
+		4'h5: segment = 7'b0010010; 	// ---m----
+		4'h6: segment = 7'b0000010; 	// |	  |
+		4'h7: segment = 7'b1111000; 	// lb	 rb
+		4'h8: segment = 7'b0000000; 	// |	  |
+		4'h9: segment = 7'b0011000; 	// ---b----
+		4'ha: segment = 7'b0001000;
+		4'hb: segment = 7'b0000011;
+		4'hc: segment = 7'b1000110;
+		4'hd: segment = 7'b0100001;
+		4'he: segment = 7'b0000110;
+		4'hf: segment = 7'b0001110;
+		4'h0: segment = 7'b1000000;
+		default: segment = 7'b1111111;
   endcase
 
 endmodule
